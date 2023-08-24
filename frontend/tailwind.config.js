95129fe51ac040e81e5f36a93a7ee7956aa7e8e6
Christoph Suessmeier <mail@suessmeier.net>
2023-08-24 11:16:01 +0200

    Added searchFood and randomFood to useLocalhost

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,13 +6,3 @@ export default {
   },
   plugins: [],
 };
-
-module.exports = {
-  theme: {
-    extend: {
-      boxShadow: {
-        'custom': '0 0 10px #ffffffb2, 0 0 15px #ffffff53, 0 0 20px #ffffff78, 0 0 25px #ffffff82, 0 0 30px #ffffff84',
-      },
-    },
-  },
-}
