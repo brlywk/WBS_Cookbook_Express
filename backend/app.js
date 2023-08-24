29525b96f40c9c816a098708757380d978b39076
Christoph Suessmeier <mail@suessmeier.net>
2023-08-24 10:52:09 +0200

    Refactored backend, added random route for hero recipe selection

--- backend/app.js
+++ backend/app.js
@@ -13,80 +13,20 @@ const db = new sqlite3.Database("./data/cookbook.sqlite", (err) => {
   }
 });
 
-// id, title, description, img
-const recipeTable = "recipes";
+// endoints: anything food related /food
+const foodRoute = require("./routes/foodRoute");
+foodRoute.setDb(db);
+app.use(foodRoute.foodRouter);
 
-// endpoint: all recipes
-app.route("/food").get((req, res, next) => {
-  const recipes = [];
+// endpoint: search for a recipe /search
+const searchRoute = require("./routes/searchRoute");
+searchRoute.setDb(db);
+app.use(searchRoute.searchRouter);
 
-  // Note: Should probably not request all recipes all the time, but good for now
-  db.all("SELECT * FROM recipes", (err, rows) => {
-    if (err || !rows) {
-      return res.status(500).send(`Error accessing database: ${err}`);
-    }
-
-    rows.forEach((r) => {
-      recipes.push({
-        id: r.id,
-        title: r.title,
-        description: r.description,
-        img: r.img,
-      });
-    });
-
-    res.status(200).json(recipes);
-  });
-});
-
-// endpoint: single recipe by id
-app.route("/food/:id").get((req, res, _next) => {
-  const { id } = req.params;
-
-  if (!id) {
-    return res.status(400).send("Error with ID");
-  }
-
-  db.get("SELECT * FROM recipes WHERE id = $id", { $id: id }, (err, row) => {
-    if (err || !row) {
-      return res.status(500).send(`Error accessing database: ${err}`);
-    }
-
-    res.status(200).send(row);
-  });
-});
-
-// endpoint: search for a recipe
-app.route("/search").get((req, res, _next) => {
-  const { query } = req.query;
-
-  if (!query) {
-    return res
-      .status(400)
-      .send("No query found in request. Please use ?query=[searchterm] to initiate a search request.");
-  }
-
-  const sqlQuery = `SELECT * FROM recipes WHERE title LIKE "%${query}%" OR description LIKE "%${query}%"`;
-
-  db.all(sqlQuery, (err, rows) => {
-    if (err) {
-      return res.status(500).send(`Error accessing database: ${err}`);
-    }
-
-    const results = [];
-
-    rows.forEach((r) => {
-      results.push({
-        id: r.id,
-        title: r.title,
-        description: r.description,
-        img: r.img,
-      });
-    });
-
-    res.status(200).json(results);
-  });
-});
+// endpoint: hero recipe /random
+const randomRoute = require("./routes/randomRoute");
+randomRoute.setDb(db);
+app.use(randomRoute.randomRouter);
 
 // start server
 const port = 3000;
---
+++ backend/routes/randomRoute.js
@@ -0,0 +1,27 @@
+const { Router } = require("express");
+const randomRouter = Router();
+
+let db;
+
+const setDb = (database) => {
+  db = database;
+};
+
+randomRouter.route("/random").get((req, res, _next) => {
+  if (!db) {
+    res.status(500).send("Error, no database connection found.");
+  }
+
+  db.get("SELECT * FROM recipes ORDER BY RANDOM() LIMIT 1", (err, row) => {
+    if (err || !row) {
+      return res.status(500).send(`Error accessing database: ${err}`);
+    }
+
+    res.status(200).json(row);
+  });
+});
+
+module.exports = {
+  randomRouter,
+  setDb,
+};
